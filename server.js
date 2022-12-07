15291ae86a2379f3564ae8499beef5fc5aa5b78f
Mahesh Sai <maheshsai252@gmail.com>
2022-12-07 03:06:18 -0500

    ad

--- server.js
+++ server.js
@@ -34,13 +34,13 @@ app.get("/", (req, res) => {
 });
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder
-  app.use(express.static('client/build'));
-  app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/')) // we're on local windows
-    url = url.substring(1);
-   res.sendFile(url);
- });
+  // app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
   // app.get('/*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
 // set port, listen for requests
